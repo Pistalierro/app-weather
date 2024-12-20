dec72c3f04d66836937d527b40f23e6eb1999e9d
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-12-20 19:09:21 +0200

    Изменил структуру приложения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {WeatherService} from './services/weather.service';
+import {CommonModule, NgIf} from '@angular/common';
 import {CurrentWeatherComponent} from './components/current-weather/current-weather.component';
 import {ForecastComponent} from './components/forecast/forecast.component';
+import {getWeatherVideoById} from './utils/getWeatherVideo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    NgIf,
+    CommonModule,
     CurrentWeatherComponent,
     ForecastComponent,
   ],
@@ -13,4 +18,14 @@ import {ForecastComponent} from './components/forecast/forecast.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  private weatherService = inject(WeatherService);
+  loading = this.weatherService.loading();
+  error = this.weatherService.error();
+
+  getCurrentVideo(): string {
+    const currentWeather = this.weatherService.currentWeather();
+    if (!currentWeather) return '/video/default.mp4';
+    return getWeatherVideoById(currentWeather.weather[0].id);
+  }
 }
