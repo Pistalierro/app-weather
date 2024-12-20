e5142bbe9f11c5d7bd1094f6c7a5635707bb82b7
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-12-20 14:46:08 +0200

    Сделал оформление и адаптивностью

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
       textShadow: {
         sm: '1px 1px 2px rgba(0, 0, 0, 0.5)', // Маленькая тень
         DEFAULT: '2px 2px 4px rgba(0, 0, 0, 0.6)', // Тень по умолчанию
-        lg: '0 0 6px rgba(0, 0, 0, 0.7)' // Большая тень
+        lg: `
+      1px 1px 0 rgba(0, 0, 0, 0.4),
+      -1px 1px 0 rgba(0, 0, 0, 0.4),
+      1px -1px 0 rgba(0, 0, 0, 0.4),
+      -1px -1px 0 rgba(0, 0, 0, 0.4),
+      3px 3px 6px rgba(0, 0, 0, 0.9)` // Черный контур и мягкая тень
       }
     }
   },
